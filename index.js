bd2add5295a4349b6ef935a29b3326daa2bc1885
ASh <ashank13mishra@gmail.com>
2026-04-12 20:56:46 +0530

    cors

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/upsc_db';
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: '*' }))
 app.use(express.json());
 
 // Routes
